90173222e3858063682c4897b220ef904ada236a
keighty <k80.leonard@gmail.com>
2016-11-20 21:49:16 -0800

    Remove encoding interference characters.

--- lib/parse_amazon_data/data_parser.rb
+++ lib/parse_amazon_data/data_parser.rb
@@ -38,11 +38,15 @@ module ParseAmazonData
     private
     def getQty(input)
       qty_regex = /([0-9]+.*)\)?/
-      interference_regex = /og\d/
-      weird = /og\d\D*(\d*\.?\d+\D*)/
-      stripped_input = input.gsub(/\(|\)/, '').downcase
+      og_interference = /og\d/
+      utf8_interference = /#x\d+;/
 
-      qty_match = if (stripped_input.match(interference_regex))
+      weird = /og\d\D*(\d*\.?\d+\D*)/
+
+      stripped_input = input.gsub(/\(|\)/, '').downcase
+      stripped_input = stripped_input.gsub(utf8_interference, "")
+
+      qty_match = if (stripped_input.match(og_interference))
         stripped_input.match(weird).captures.first
       else
         stripped_input.match(qty_regex).to_s
